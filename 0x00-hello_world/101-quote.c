30e67024dab4f35abb672ffc757f6245810f782d
titusthuo <mwangititus6634@gmail.com>
2023-02-16 10:03:38 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to a standard error
+ * main - A program that prints a line to a standard error
  * Return: 1 (Success)
  */
 int main(void)
