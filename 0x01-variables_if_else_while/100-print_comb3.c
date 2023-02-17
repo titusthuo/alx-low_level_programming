c6cf9c1afb5b546caecd52cfe9e013d3fa449012
titusthuo <mwangititus6634@gmail.com>
2023-02-17 08:26:45 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
 
-			if (i < 8)
+			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
