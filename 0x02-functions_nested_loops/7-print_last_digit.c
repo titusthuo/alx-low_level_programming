3fb5f997628d6d6535963ae3b9b22e6a8a5b04cc
titusthuo <mwangititus6634@gmail.com>
2023-02-21 12:28:53 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,14 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int ld;
 
+	if (n < 0)
+		n = -n;
+	ld = n % 10;
 	if (ld < 0)
-		ld *= -1;
-
+		ld = -ld;
 	_putchar(ld + '0');
 
-	return (0);
+	return (ld);
 }
