5d060a91a54002359f2ae1e4822d577e48c80a03
titusthuo <mwangititus6634@gmail.com>
2023-03-10 12:36:13 -0800

    4-strpbrk.c

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,10 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
 char *_strpbrk(char *s, char *accept)
 {
-	return strpbrk(s, accept);
+	int t;
+
+	while (*s)
+	{
+		for (t = 0; accept[t]; t++)
+		{
+			if (*s == accept[t])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
 }
