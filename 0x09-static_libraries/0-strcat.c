9a54899a7ac95616cc9d54e53dc96fb676ae8a5c
titusthuo <mwangititus6634@gmail.com>
2023-03-10 12:01:19 -0800

    0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,10 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+/**
+ * _strcat -> this is a function stract
+ * @dest: first param
+ * @src: second param
+ *
+ * Return: a string
+ */
 char *_strcat(char *dest, char *src)
 {
-	return strcat(dest src);
+	int len = 0, i;
+
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+
+	dest[len] = '\0';
+	return (dest);
 }
