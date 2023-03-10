60b85b15c4b6e285e82b9f4416078425629c7632
titusthuo <mwangititus6634@gmail.com>
2023-03-10 12:43:02 -0800

    5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,10 +1,36 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to be searched
+ * @needle: the substring to be located
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found.
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	return strstr(haystack, needle);
+	int t;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		t = 0;
+
+		if (haystack[t] == needle[t])
+		{
+			do {
+				if (needle[t + 1] == '\0')
+					return (haystack);
+
+				t++;
+
+			} while (haystack[t] == needle[t]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
 }
