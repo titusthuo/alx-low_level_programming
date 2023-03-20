5031e73335c353d7b0a269f90d30e434b1711d16
titusthuo <mwangititus6634@gmail.com>
2023-03-20 06:53:03 -0700

    0-main.c

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,17 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int j;
+
+	j = 98 + SIZE;
+	printf("%d\n", j);
+	return (0);
+}
